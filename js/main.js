c91027d2ec96128671ffef0da353e8ed3ae6efc8
oh108899 <oh108899@gmail.com>
2025-11-24 17:48:38 +0900

    feat:슬라이드 js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ window.addEventListener('scroll',()=>{
     btt.classList.remove('active');
   }
 })
+
+//슬라이드
+const tester = document.querySelector('.testimonial');
+const testerUl = tester.querySelector('ul');
+const testerLi = testerUl.querySelectorAll('li');
+const pbt = tester.querySelector('.prev');
+const nbt = tester.querySelector('.next');
+let curIdx = 0;
+
+function move(idx){
+  testerUl.style.left = idx * -100 + '%';
+  curIdx = idx;
+}
+nbt.addEventListener('click',()=>{
+  let next = (curIdx + 1)% testerLi.length;
+  move(next);
+})
+pbt.addEventListener('click',()=>{
+  let next = (curIdx - 1 + testerLi.length)% testerLi.length;
+  move(next);
+})
+
+// 아코디언
